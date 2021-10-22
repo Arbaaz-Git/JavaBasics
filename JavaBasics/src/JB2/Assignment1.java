e89b0a584807158805e35d5f655e6fdb22293d3b
ArbaazKhan1 <bazzy_94@yahoo.com>
2021-10-22 13:22:28 -0500

    Updated Assignemnt 2 in JB2 with comments about how to run the code.

--- JavaBasics/src/JB2/Assignment1.java
+++ JavaBasics/src/JB2/Assignment1.java
@@ -6,6 +6,8 @@ package JB2;
 /**
  * @author Arbaaz Khan
  * This program will be taking multiple command line inputs and outputting  the sum of them. The program will being looking for any integer or float value and adding together. The program will also ignore all non int/float inputs. A key point to note is that this code does not handle overflow issues. 
+ * 
+ * Important! Must run code with command line arguments. If you are running the code in and IDE then make sure when run the code that the IDE is inputting arguments to run. If running in Eclipse then click the drop-down arrow on the run button and then go to run configurations. In the run configurations window, click on the arguments tab and filling in the program argumets text box with your desired inputs. Then hit apply and run. 
  *
  */
 public class Assignment1 {
