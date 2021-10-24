017086ec38fa023c8e32d9df946997cef815814f
ArbaazKhan1 <bazzy_94@yahoo.com>
2021-10-24 18:35:23 -0500

    Created a resources folder, with text files inside of it. Completed Assignment 2.

--- JavaBasics/src/JB3/Assignment2.java
+++ JavaBasics/src/JB3/Assignment2.java
@@ -3,8 +3,15 @@
  */
 package JB3;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * @author Arbaaz Khan
+ * The goal of this assignment is to append text to an existing file. The file that is going to be append is called File_to_Append.txt
  *
  */
 public class Assignment2 {
@@ -13,8 +20,25 @@ public class Assignment2 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Assignment2 assign =  new Assignment2();
+		//The user can enter whatever they want into the file 
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter text to append to the file: ");
+		String s = scan.nextLine();
+		
+		assign.appendFile(s);
 
 	}
+	
+	public void appendFile(String s) {
+		String file = "resources/File_to_Append.txt";
+		try(BufferedWriter buffer = new BufferedWriter(new FileWriter(file,true))){	//Create a buffered writer to write to the file
+		    buffer.write(s+"\n");   
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+
 
 }
